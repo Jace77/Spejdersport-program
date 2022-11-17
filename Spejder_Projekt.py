fa4de1b89622b084b95e25393701185f7ca672d9
Jace77 <rasmus102030@gmail.com>
2022-11-17 21:11:52 +0100

    Update Spejder_Projekt.py

--- Spejder_Projekt.py
+++ Spejder_Projekt.py
@@ -2,30 +2,30 @@
 import PySimpleGUI as sg
 
 
-layout = [[sg.Text('Skriv dit fulde navn:', size=(20, 1)), sg.InputText()],
-          [sg.Text('Skriv din email:     ', size=(20, 1)), sg.InputText()],
-          [sg.Text('Skriv dit telefon nr:', size=(20, 1)), sg.InputText()],
-          [sg.Text('Skriv din adresse:   ', size=(20, 1)), sg.InputText()],
-          [sg.Button('Save'), sg.Button('Luk')]]
+layout = [[sg.Text('write your full name:', size=(20, 1)), sg.InputText()],
+          [sg.Text('write your email:     ', size=(20, 1)), sg.InputText()],
+          [sg.Text('write your phone nr.:', size=(20, 1)), sg.InputText()],
+          [sg.Text('write your adress:   ', size=(20, 1)), sg.InputText()],
+          [sg.Button('Save'), sg.Button('close')]]
 
 window = sg.Window('Spejdersport', layout, margins=(100, 100))
 window.read()
 
 while True:
     event, values = window.read()
-    if event == 'Luk' or event == sg.WINDOW_CLOSED:
+    if event == 'close' or event == sg.WINDOW_CLOSED:
         break
     if event == 'Save':
         with open('Brugerfile.txt', 'w') as f:
-            f.write('Navn: ')
+            f.write('Name: ')
             f.write(values[0])
             f.write('\n')
             f.write('Email: ')
             f.write(values[1])
             f.write('\n')
-            f.write('Tlf nr: ')
+            f.write('phone nr: ')
             f.write(values[2])
             f.write('\n')
-            f.write('Adresse: ')
+            f.write('Adress: ')
             f.write(values[3])
         break
